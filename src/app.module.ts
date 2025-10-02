0e79d4e9b9520c46716716a0afca72dffaa0df08
Gustavo <gustavoalt93@gmail.com>
2025-10-02 15:42:04 -0300

    limpio

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { HttpModule } from '@nestjs/axios';
 import { GifModule } from './gif/gif.module';
 
 @Module({
   imports: [ConfigModule.forRoot(), HttpModule, GifModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
